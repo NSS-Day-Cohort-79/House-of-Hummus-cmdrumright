eeed7fad0c76d54a1f6c90b4a590e2fed79b206c
cmdrumright <cdrumright@protonmail.com>
2025-12-15 15:00:29 -0600

    imported setSide to SideDishes.js and added changeHandler to invoke

--- scripts/SideDishes.js
+++ scripts/SideDishes.js
@@ -1,6 +1,16 @@
+import { setSide } from "./transientState.js"
+
+const handleChange = (e) => {
+    if (e.target.name === "side") {
+        setSide(parseInt(e.target.value))
+    }
+}
+
 export const sideList = async () => {
     const response = await fetch("http://localhost:8088/sides")
     const options = await response.json()
+
+    document.addEventListener("change", handleChange)
 
     let html = `
         <div class="options">
